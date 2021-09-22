9312ed73758d8d143f699dead868428f88ac57fa
Kanan <kenan@luxar.se>
2021-09-22 12:28:02 +0200

    Grundstruktur

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WebShop_Stripe
 ett projektarbete för att bygga enkel webbshop med "Stripe" betalningsmetod
+
+
+har instalerade 
+
+npm install express --save // för server del
+npm install dotnev // för .env fil att skydda STRIPE_SECRET_KEY 
+npm install stripe --save //och själva Stripe 
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+require('dotenv').config('.env')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const app = express();
+
+app.use('/api', express.json())
+
+// Endpoints
+app.post('/api/checkout-session', async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+            {
+                description: '',
+                price_data: {
+                currency: "sek",
+                product_data: {
+                    name: "",
+                },
+                unit_amount: 10000,
+                },
+                quantity: 2,
+            },
+            ],
+            mode: "payment",
+            success_url: "http://localhost:3000/some.html",
+            cancel_url: "http://localhost:3000",
+        });
+        res.json({ id: session.id })
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error })
+    }
+})
+
+app.post('/api/verify-checkout-session', async (req, res) => {
+    try {
+        const session = await stripe.checkout.sessions.retrieve(req.body.sessionId)
+        //console.log(session)
+        if(session) {
+            res.send({ isVerified: true })
+        } else {
+            throw new Error('no session')
+        }
+    } catch (error) {
+        //console.error(error)
+        //res.send({ isVerified: false });        
+    }
+});
+
+app.use(express.static('public'))
+
+app.listen(3000, () => console.log('Server is on LIVE'))
