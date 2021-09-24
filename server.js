d6b7e0335dad8fc4bb792b510af21988726ed827
Ryan-Phillips Cornelio <ryan.cornelio@gmail.com>
2021-09-24 11:50:36 +0200

    cancel

--- server.js
+++ server.js
@@ -1,56 +1,58 @@
-const express = require('express')
-require('dotenv').config('.env')
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const express = require("express");
+require("dotenv").config(".env");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
-app.use('/api', express.json())
+app.use("/api", express.json());
 
 // Endpoints
-app.post('/api/checkout-session', async (req, res) => {
-    try {
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            line_items: [
-            {
-                description: 'test',
-                price_data: {
-                currency: "sek",
-                product_data: {
-                    name: "test",
-                },
-                unit_amount: 10000,
-                },
-                quantity: 2,
+app.post("/api/checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          description: "test",
+          price_data: {
+            currency: "sek",
+            product_data: {
+              name: "test",
             },
-            ],
-            mode: "payment",
-            success_url: "http://localhost:3001/confirmation/?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: "http://localhost:3001",
-        });
-        res.json({ id: session.id })
-        console.log(res.json)
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ error })
-    }
-})
-
-app.post('/api/verify-checkout-session', async (req, res) => {
-    try {
-        const session = await stripe.checkout.sessions.retrieve(req.body.sessionId)
-        console.log(session)
-        if(session) {
-            res.send({ isVerified: true })
-        } else {
-            throw new Error('no session')
-        }
-    } catch (error) {
-        console.error(error)
-        res.send({ isVerified: false });        
-    }
+            unit_amount: 10000,
+          },
+          quantity: 2,
+        },
+      ],
+      mode: "payment",
+      success_url:
+        "http://localhost:3001/confirmation/?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url:
+        "http://localhost:3001/cancel/?session_id={CHECKOUT_SESSION_ID}",
+    });
+    res.json({ id: session.id });
+    console.log(res.json);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 });
 
-app.use(express.static('public'))
+app.post("/api/verify-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.retrieve(req.body.sessionId);
+    console.log(session);
+    if (session) {
+      res.send({ isVerified: true });
+    } else {
+      throw new Error("no session");
+    }
+  } catch (error) {
+    console.error(error);
+    res.send({ isVerified: false });
+  }
+});
 
-app.listen(3001, () => console.log('Server is on LIVE'))
+app.use(express.static("public"));
+
+app.listen(3001, () => console.log("Server is on LIVE"));
