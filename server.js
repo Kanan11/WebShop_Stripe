d4a5854ab0b837491ce2864fdd178e409034f5be
Kanan <kenan@luxar.se>
2021-09-29 13:05:46 +0200

    Ny Style av Pizzeria

--- server.js
+++ server.js
@@ -9,30 +9,36 @@ app.use('/api', express.json())
 // Endpoints
 app.post('/api/checkout-session', async (req, res) => {
     try {
-        const { product } = req.body;
-
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            line_items: [
-            {
-                description: product.description,
-                price_data: {
+        const { cart } = req.body;
+        console.log(cart)
+        let carts = []
+        cart.forEach((cart) => {
+            let lineItem = {
+              description: '.',
+              price_data: {
                 currency: "sek",
                 product_data: {
-                    name: product.name,
-                    images: [product.image],
+                  name: cart.title,
+                  images: [cart.image],
                 },
-                unit_amount: product.amount * 100,
-                },
-                quantity: product.quantity,
-            },
-            ],
+                unit_amount: cart.price * 100,
+              },
+              quantity: cart.amount,
+            };
+            carts.push(lineItem);
+          });
+
+        console.log(carts)
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: carts,
             mode: "payment",
             success_url: "http://localhost:3001/confirmation/?session_id={CHECKOUT_SESSION_ID}",
             cancel_url: "http://localhost:3001",
         });
-        res.json({ id: session.id })
-        
+
+        res.status(201).json({ id: session.id })
+
     } catch (error) {
         console.error(error)
         res.status(500).json({ error })
@@ -43,14 +49,14 @@ app.post('/api/verify-checkout-session', async (req, res) => {
     try {
         const session = await stripe.checkout.sessions.retrieve(req.body.sessionId)
         console.log(session)
-        if(session) {
+        if (session) {
             res.send({ isVerified: true })
         } else {
             throw new Error('no session')
         }
     } catch (error) {
         console.error(error)
-        res.send({ isVerified: false });        
+        res.send({ isVerified: false });
     }
 });
 
